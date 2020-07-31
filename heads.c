382c58ee8124d2badad55be5ea7bc00244bc8f2c
Ryoma <heroku@nowhere.co.jp>
2020-07-31 10:37:49 +0900

    HoT:Initial revision.

---
+++ heads.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    int headsOrTails[5]={0};
+    int counterForHeads=0;
+    srand((unsigned)time(NULL));
+
+    for(int i=0;i<5;i++){
+        headsOrTails[i]=rand()%2;
+        if(i<3&&headsOrTails[i]==0){
+            counterForHeads++;
+        }
+    }
+    for(int i=0;i<3;i++){
+        printf("Round %d: %s\n",i+1,headsOrTails[i]==0?"Heads":"Tails");
+    }
+    printf("Heads: %d, Tails: %d\n",counterForHeads,3-counterForHeads);
+
+    return 0;
+}
